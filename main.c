b24ab09fb7e8fa1f96380b93d9d661e9d5bfee39
MarioSotoF <sot18351@uvg.edu.gt>
2021-05-14 00:31:31 -0600

    Initial commit

---
+++ main.c
@@ -0,0 +1,27 @@
+/*
+ * Nombre: Mario Soto
+ * Carnet: 18351
+ * Laboratorio 7
+ * Catedratico: Pablo Mazariegos
+ */
+
+//Agregamos las librerias necesarias
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/tm4c123gh6pm.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_ints.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/gpio.h"
+#include "driverlib/timer.h"
+
+/**
+ * main.c
+ */
+int main(void)
+{
+	return 0;
+}
